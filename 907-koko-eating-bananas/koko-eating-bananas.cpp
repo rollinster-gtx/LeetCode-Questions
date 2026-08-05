71cae2042b5c9f96ee87459c27c12e3bcfd2f1c0
rollinster-gtx <swapnilpandeyiitias@gmail.com>
2026-08-06 02:10:01 +0530

    Time: 8 ms (54.43%) | Memory: 23 MB (19.79%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+
+        int start = 1;
+        int end = *max_element(piles.begin(), piles.end());
+        int ans = end;
+
+        while (start <= end) {
+
+            int mid = start + (end - start) / 2;
+
+            long long hours = 0;
+
+            for (int i = 0; i < piles.size(); i++) {
+                hours += (piles[i] + mid - 1) / mid;
+            }
+
+            if (hours <= h) {
+                ans = mid;
+                end = mid - 1;
+            }
+            else {
+                start = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+};
