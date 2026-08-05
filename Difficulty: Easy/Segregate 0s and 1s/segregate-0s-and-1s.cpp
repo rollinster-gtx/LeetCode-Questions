cad4ee0c12c310c14ddf339ab1e3fc10917367e6
rollinster-gtx <swapnilpandeyiitias@gmail.com>
2026-08-06 02:20:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Segregate 0s and 1s/segregate-0s-and-1s.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    void segregate0and1(vector<int> &arr) {
+        // code here
+        int start=0,end=arr.size()-1;
+        while(start<=end)
+        {
+            if(arr[start]==0)
+            start++;
+            else
+            {
+                if(arr[end]==0)
+                {
+                    swap(arr[start],arr[end]);
+                    start++,end--;
+                }
+                else
+                end--;
+            }
+        }
+    }
+};
