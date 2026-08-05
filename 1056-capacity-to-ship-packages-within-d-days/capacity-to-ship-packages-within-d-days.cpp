50da8395150b5d357c96e479905aad2a28bc3fce
rollinster-gtx <swapnilpandeyiitias@gmail.com>
2026-08-06 02:02:18 +0530

    Time: 10 ms (69.93%) | Memory: 35.1 MB (54.92%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+         long long start=0,end=0,mid,ans;
+        for(int i=0;i<weights.size();i++)
+        {
+            start=max(start,(long long)weights[i]);
+            end+=weights[i];
+        }
+        
+        while(start<=end)
+        {
+            mid=start+(end-start)/2;
+            
+            int sum=0,count=1;
+            for(int i=0;i<weights.size();i++)
+            {
+                sum+=weights[i];
+                if(sum>mid)
+                {
+                    count++;
+                    sum=weights[i];
+                }
+            }
+            
+            if(count<=days)
+            {
+                ans=mid;
+                end=mid-1;
+            }
+            else
+            start=mid+1;
+        }
+        return ans;
+
+        
+    }
+};
