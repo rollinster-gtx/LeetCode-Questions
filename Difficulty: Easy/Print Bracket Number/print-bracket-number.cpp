455e3d156f9a01d212227bbfb2805bbe2dfb04c5
rollinster-gtx <swapnilpandeyiitias@gmail.com>
2026-08-25 00:17:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Print Bracket Number/print-bracket-number.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+
+    vector<int> bracketNumbers(string &s) {
+        // code here
+        int count = 0;
+        stack<int>st;
+        vector<int>ans;
+        
+        for(int i=0;i<s.size();i++)
+        {
+            //opening bracket
+            if(s[i]=='(')
+            {
+                count++;
+                st.push(count);
+                ans.push_back(count);
+            }
+            //closing bracket
+            else if(s[i]==')')
+            {
+                ans.push_back(st.top());
+                st.pop();
+                
+            }
+        }
+        return ans;
+    }
+};
